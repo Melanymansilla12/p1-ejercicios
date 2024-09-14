32ed36fe6c9b979fb72f6fbb7b10097e42d3febb
Melanymansilla12 <Melany.mansilla@davinci.edu.ar>
2024-09-14 15:20:16 -0300

    termine ejercicio 4

--- 2024-02-clase02-ejercicio4/index.js
+++ 2024-02-clase02-ejercicio4/index.js
@@ -1,61 +1,155 @@
-'ise strict'
+'use strict'
 
-let ciudad,edad,sexo
-
-
-let contadorEdadMujer=0,conradorEdadHombre=0,contadorEdad=0;
-let acumuladorEdadMujer=0,acumuladorEdadhombre=0,acumuladorEdad=0;
-
+let ciudad,edad,sexo;
 let datoValido=true;
 
-let edadMinima=Infinity;
-let edadMaxima=-Infinity;
+//variables de promedio
+let contadorMujer=0,contadorHombre=0,contadorGeneral=0,contadorNoBinario=0;
+let acumuladorEdadMujer=0,acumuladorEdadhombre=0,acumuladorEdadGeneral=0,acumuladorEdadNoBinario=0;
 
-do {
-    ciudad=prompt ("ingrese su ciudad");
+let contadorMujeresMenores21=0,contadorHombresMayor21=0;
 
+//minimos y maximos
+let edadhombreMin=110, edadMujerMax=0; //le pongo el limite del rango directo 
 
-//validación de ciudad
-
-if(isNaN(ciudad)){
+do{
+    do {
+        ciudad=prompt ("ingrese su ciudad"); 
+    
+    
+    //validación de ciudad
+    
+    if(!isNaN(ciudad)){
+        alert("ingrese texto");
+        datoValido=false;
+    }
+    
+    else if (ciudad===null){
+        alert("Por favor ingrese un dato")
+        datoValido=False;
+    }
+    
+    else if(ciudad.trim()===""){
+        alert("no dejar el recuadro vacio");
+    }
+    
+    else {datoValido=true}
+    
+    }while(!datoValido);
+    
+    //validación sexo
+    
+    do {
+        sexo=prompt ("ingrese su sexo (F, M o X)"); //Si yo pongo tolowercase cuando el usuario apreta cancell rompe
+    
+    if(!isNaN(sexo)){ //!nan sirve para que si algo es un numero de true y se vuelva a ejecutar
     alert("ingrese texto");
     datoValido=false;
-}
-
-else if (ciudad===null){
-    alert("Por favor ingrese un dato")
-    datoValido=False;
-}
-
-else if(ciudad.trim()===""){
-    alert("no dejar el recuadro vacio");
-}
-
-else {datoValido=true}
-
-}while(!datoValido);
-
-//validación sexo
-
-do {
-    sexo=promt ("ingrese su sexo (F o M)");
-
-if(isNaN(sexo)){
-alert("ingrese texto");
-datoValido=false;
-}
-
-else if(sexo===null){
-alert("ingrese un dato");
-datoValido=false;
-}
+    }
     
-else if(sexo.trim()===""){
-    alert("no dejar el recuadro vacio");
+    else if(sexo===null){
+    alert("ingrese un dato");
+    datoValido=false;
+    }
+        
+    else if(sexo.trim()===""){ //uso el trim para sacar los espacios del principio y del final
+        alert("no dejar el recuadro vacio");
+    }
+    
+    else if(sexo.toLowerCase()!=="m" && sexo.toLowerCase()!=="f" && sexo.toLowerCase()!=="x" ) {
+        alert ("complete con una opcion válida (F,M,X)");
+        datoValido= false;
+    } else{
+        datoValido=true;
+    }
+    
+    }while(!datoValido);
+    
+    
+    //validamos edad
+    
+    do {
+        edad= parseInt (prompt ("ingrese una edad entre 0 y 110"));
+    
+        if(isNaN (edad)){
+            alert("ingrese numeros");
+            datoValido= false;
+        }
+    
+        else if (edad<0 || edad > 110){
+            alert ("Edad fuera de rango. Ingrese entre 0 y 110");
+            datoValido= false;
+        }
+        else{
+            datoValido= true;
+        }
+    
+    } while (!datoValido);
+
+    //aca ya esta validado
+
+// siempre se incrementa en contador general
+contadorGeneral++;
+acumuladorEdadGeneral += edad;
+
+
+if(sexo.toLowerCase()==="m"){
+    contadorHombre++;
+    acumuladorEdadhombre += edad;
+
+    //requisito edad
+    if(edad>21){
+        contadorHombresMayor21++;
+    }
+
+    //busco minimo entre hombres 
+    if(edad < edadhombreMin){
+        edadhombreMin=edad; //si el valor es maximo lo pisa al anterior y queda ese valor
+    }
+
+}
+else if (sexo.toLowerCase()==="f"){
+    contadorMujer++;
+    acumuladorEdadMujer += edad;
+
+    //requisito edad
+    if(edad<21){
+        contadorMujeresMenores21++;
+    }
+
+    //busco maximo entre mujeres
+
+    if(edad > edadMujerMax){
+        edadMujerMax=edad; //si el valor es maximo lo pisa al anterior y queda ese valor
+    }
+     
+} else {
+    contadorNoBinario++;
+    acumuladorEdadNoBinario += edad; 
 }
 
-else if(sexo.toLowerCase()!==m && sexo.toLowerCase()!==f && sexo.toLowerCase()!==x ) {
-    alert ("complete con una opcion válida (F,M,X)");
-}
 
-}while(!datoValido);
+
+} while (confirm ("¿otro dato?"))
+
+
+//ponemos en la consola para no tener tantos alert
+
+console.log (`Promedio general: ${acumuladorEdadGeneral/contadorGeneral}`);
+console.log(`Promedio edad hombres: ${acumuladorEdadhombre/contadorHombre}`);
+console.log(`Promedio edad Mujeres: ${acumuladorEdadMujer/contadorMujer}`);
+console.log(`Promedio edad no binario: ${acumuladorEdadNoBinario/contadorNoBinario}`);
+
+//con el to.fix le saco las comas a los numeros
+
+
+
+//aca hago porcentaje
+console.log (`un %${100*contadorHombresMayor21/contadorGeneral} de personas son hombres mayores a 21`);
+ console.log(`un %${100 * contadorMujeresMenores21/contadorGeneral} de personas son mujeres menores a 21`);
+
+
+//minimos y maximos
+
+console.log(`La edad maxima de mujeres es de ${edadMujerMax} años`);
+console.log(`La edad minima de hombres es de ${edadMujerMax} años`);
