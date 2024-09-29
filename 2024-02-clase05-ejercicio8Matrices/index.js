63c44ece44cd9417b26a5b9bae65a982721abc2a
Melanymansilla12 <Melany.mansilla@davinci.edu.ar>
2024-09-29 15:01:39 -0300

    .

--- 2024-02-clase05-ejercicio8Matrices/index.js
+++ 2024-02-clase05-ejercicio8Matrices/index.js
@@ -1,17 +1,21 @@
 //variablepara tablero batalla navalq
 let tablero=[];
 
-
+//indices para las filas
+let filas=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
 //CREO 10 FILAS EN TOTAL
 for (let i=0; i<10; i++){ //ARRANCA EL BUCLE
     let fila=[];// CREA UNA FILA VACIA
 
-    for(let i=0; i<10; i++) { 
+    for(let j=0; j<10; j++) { //columnas y i filas
 
         fila.push(""); //LLENA CON 10 ESPACIOS
     }
-    
+    //Consigo la letra de la fila
+    let letra=filas[i];
+    //agrego la fila que arme al tablero
+    tablero[letra]=fila;
 
 
 
@@ -26,8 +30,8 @@ function agregarBarco(){
 let largo=parseInt(prompt("Cual es el largo del barco"));
 //PREGUNTO POR FILA Y COLUMNA COMO INDIQUE EL LARGO DEL BARCO
     for (let i=0; i<largo; i++){
-        let fila=parseInt(prompt("en que fila esta el barco?"));
-        let col= parseInt(prompt("en que columna va el barco?"));
+        let fila=prompt("en que fila esta el barco?");
+        let col= parseInt(prompt("en que columna va el barco?")) -1 ;
     
 
     //asigno el barco en la fila y columna
@@ -36,7 +40,7 @@ let largo=parseInt(prompt("Cual es el largo del barco"));
 }
 
 function disparar(){
-    let fila=parseInt(prompt("a que fila queres disparar?"));
+    let fila=prompt("a que fila queres disparar?");
     let col= parseInt(prompt("a que columna queres disparar?"));
 
     if(tablero[fila][col]==="#"){
@@ -55,7 +59,7 @@ function disparar(){
 function mostrar (){
     for(let fila of tablero){
 
-        console.log(fila);
+        console.log (tablero[fila]);
     }
 
 }
